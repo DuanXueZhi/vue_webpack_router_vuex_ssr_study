520aba904d32c723891052bc32ce1eba405edd63
DuanXueZhi <38554820+DuanXueZhi@users.noreply.github.com>
2020-08-10 17:59:36 +0800

    webpack：webpack配置目录分文件（bindling报错、VueLoaderPlugin提示修改），依赖位置调整

---
+++ vue.js-todolist-master/build/webpack.config.base.js
@@ -0,0 +1,53 @@
+/**
+ *  * Created by dxz on 2020/8/10-14:58.
+ *  explain：所有webpack（环境）依赖的配置
+ */
+const path = require('path')
+
+const config = {
+  //入口， __dirname 是当前文件所在目录
+  entry: path.join(__dirname, '../src/index.js'),
+  //输出
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, 'dist')
+  },
+  //webpack原生只支持js文件类型，只支持ES5语法，我们使用以.vue文件名结尾的文件时，需要为其指定loader
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: __dirname + 'node_modules', // 排除node_modules内容 __dirname写法防止'bindings' of null报错
+        include: __dirname + 'src',
+        options: {
+          presets: ['env']
+        }
+      },
+      //将小于1024d的图片转为base64，减少http请求
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name]-[hash:8].[ext]',
+              outputPath: 'assets/img/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
---
+++ vue.js-todolist-master/build/webpack.config.client.js
@@ -0,0 +1,111 @@
+/**
+ *  * Created by dxz on 2020/8/10-15:00.
+ *  explain：扩展webpack配置
+ */
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // webpack4相关 == webpack3 extract-text-webpack-plugin
+const baseConfig = require('./webpack.config.base')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const definePlugins = [
+  // 该插件帮助我们安心地使用环境变量
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: isDev ? '"development"' : '"production"'
+    }
+  }),
+  new HTMLPlugin()
+]
+
+const devServer = { // 端口ip：默认8080，localhost
+  // port: 9000,
+  // host: '127.0.0.1',
+  overlay: {
+    errors: true
+  },
+  hot: true
+}
+
+let config
+
+if (isDev) { // 开发环境 // 合理合并base中的配置
+  config = merge(baseConfig, {
+    module: {
+      rules: [
+        {
+          //css预处理器，使用模块化的方式写css代码
+          //stylus-loader专门用来处理stylus文件，处理完成后变成css文件，交给css-loader.webpack的loader就是这样一级一级向上传递，每一层loader只处理自己关心的部分
+          test: /\.styl/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true }
+            },
+            'stylus-loader'
+          ]
+        }
+      ]
+    },
+    devServer,
+    plugins: definePlugins.concat([
+      new webpack.HotModuleReplacementPlugin(),
+      new VueLoaderPlugin(), // 与vue-loader有关，不引入则报错：vue-loader was used without the corresponding plugin. Make sure to include VueLoaderPlugin in your webpack config. 提示引入
+      // new webpack.NoEmitOnErrorsPlugin() // webpack4 内置
+    ])
+  })
+} else { // 正式环境
+  config = merge(baseConfig, {
+    entry: {
+      app: path.join(__dirname, '../src/index.js'),
+      vendor: ['vue']
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js'
+    },
+    module: {
+      rules: [
+        //css预处理器，使用模块化的方式写css代码
+        //stylus-loader专门用来处理stylus文件，处理完成后变成css文件，交给css-loader.webpack的loader就是这样一级一级向上传递，每一层loader只处理自己关心的部分
+        {
+          test: /\.styl/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it uses publicPath in webpackOptions.output
+                publicPath: './',
+                hmr: process.env.NODE_ENV === 'development',
+              },
+            },
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true }
+            },
+            'stylus-loader'
+          ]
+        }
+      ]
+    },
+    plugins: definePlugins.concat([
+      new VueLoaderPlugin(), // 与vue-loader有关，不引入则报错：vue-loader was used without the corresponding plugin. Make sure to include VueLoaderPlugin in your webpack config. 提示引入
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // all options are optional
+        filename: 'styles.[chunkhash].[name].css',
+        chunkFilename: '[id].css',
+        ignoreOrder: false, // Enable to remove warnings about conflicting order
+      })
+    ])
+  })
+}
+
+module.exports = config
