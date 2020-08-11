867eba5a5c069a0747998ce76d252986db0cf4ae
DuanXueZhi <38554820+DuanXueZhi@users.noreply.github.com>
2020-08-11 09:33:00 +0800

    webpack：格式调整

--- vue.js-todolist-master/client/views/todo/todo.vue
+++ vue.js-todolist-master/client/views/todo/todo.vue
@@ -1,95 +1,95 @@
 <template>
-    <section class="real-app">
-        <input 
-            type="text" 
+  <section class="real-app">
+    <input
+            type="text"
             class="add-input"
             autofocus="autofocus"
             placeholder="接下来做什么"
             @keyup.enter="addTodo"
-        >
-        <Item  
+    >
+    <Item
             v-for="todo in filterTodos"
             :todo="todo"
             :key="todo.id"
             @del="deleteTodo"
-        />
-        <Tabs 
-            :filter="filter" 
+    />
+    <Tabs
+            :filter="filter"
             :todos="todos"
             @toggle="toggleFilter"
             @clearAll="clearAllCompletedTodo"
-        />
-    </section>
+    />
+  </section>
 </template>
 
 <script>
-import Item from './item.vue';
-import Tabs from './tabs.vue';
-import { constants } from 'crypto';
+  import Item from './item.vue';
+  import Tabs from './tabs.vue';
+  import {constants} from 'crypto';
 
-let id = 0;
+  let id = 0;
 
-export default {
+  export default {
     data() {
-        return {
-            todos: [],
-            filter: 'all'
-        }
+      return {
+        todos: [],
+        filter: 'all'
+      }
     },
     components: {
-        Item,
-        Tabs
+      Item,
+      Tabs
     },
     computed: {
-        filterTodos() {
-            if(this.filter === 'all') {
-                return this.todos;
-            }
-            const filterCompleted = this.filter === 'completed';
-            return this.todos.filter(todo=> todo.completed===filterCompleted);
+      filterTodos() {
+        if (this.filter === 'all') {
+          return this.todos;
         }
+        const filterCompleted = this.filter === 'completed';
+        return this.todos.filter(todo => todo.completed === filterCompleted);
+      }
     },
     methods: {
-        addTodo(e) {
-            this.todos.unshift({
-                id: id++,
-                content: e.target.value,
-                completed: false
-            });
+      addTodo(e) {
+        this.todos.unshift({
+          id: id++,
+          content: e.target.value,
+          completed: false
+        });
 
-            e.target.value = '';
-        },
-        deleteTodo(id) {
-            this.todos.splice(this.todos.findIndex(todo => id === todo.id), 1);
-        },
-        toggleFilter(state) {
-            console.log(state);
-            this.filter = state;
-        },
-        clearAllCompletedTodo() {
-            this.todos = this.todos.filter(todo=> todo.completed===false);
-        }
+        e.target.value = '';
+      },
+      deleteTodo(id) {
+        this.todos.splice(this.todos.findIndex(todo => id === todo.id), 1);
+      },
+      toggleFilter(state) {
+        console.log(state);
+        this.filter = state;
+      },
+      clearAllCompletedTodo() {
+        this.todos = this.todos.filter(todo => todo.completed === false);
+      }
     }
-}
+  }
 </script>
 
 <style lang="stylus" scoped>
-.real-app
+  .real-app
     width 600px
     margin 0 auto
     box-shadow 0 0 5px #666
 
-.add-input
+  .add-input
     position relative
     margin 0
     width 100%
     font-size 24px
     font-family inherit
-    font-weight inherit 
+    font-weight inherit
     line-height 1.4em
     border none
-    outline none 
-    color inherit 
+    outline none
+    color inherit
     box-sizing border-box
     font-smoothing antialiased
     padding 16px 16px 16px 36px
