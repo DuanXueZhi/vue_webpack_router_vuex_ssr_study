e75579a0bde5bdf1a2fba594a4d54af2b3c18b45
DuanXueZhi <38554820+DuanXueZhi@users.noreply.github.com>
2020-08-11 16:32:54 +0800

    webpack：vue中style标签cssModules配置（已失效，后期需要修改）

--- vue.js-todolist-master/build/vue-loader.config.js
+++ vue.js-todolist-master/build/vue-loader.config.js
@@ -8,7 +8,10 @@ module.exports = (isDev) => {
   return {
     preserveWhitepace: true, // 去除html中多余的空格（空格默认渲染）
     extractCSS: !isDev, // vue中的css提取到css单独打包文件
-    cssModules: {},
+    cssModules: {
+      localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]', // 设置类名
+      camelCase: true // 将css中变量名横杠连接方式改为驼峰命名
+    },
     // postcss 全局已配置
     hotReload: true, // 是否热重载（默认true：可局部更新，false：刷新整个页面更新【可能有vue-style-loader重载style样式】）
     loaders: { // 指定loader解析自定义docs标签
--- vue.js-todolist-master/client/layout/header.vue
+++ vue.js-todolist-master/client/layout/header.vue
@@ -1,10 +1,23 @@
 <template>
-  <header class="main-header">
+  <header :class="$style.mainHeader">
     <h1>Todo</h1>
   </header>
 </template>
 
-<style lang="stylus" scoped>
+<script>
+  export default {
+    // computed: { // 相当于在计算属性中增加$style
+    //   $style() {
+    //     return {
+    //       minHeader: ''
+    //     }
+    //   }
+    // }
+  }
+</script>
+
+<style lang="stylus" module>
+  // module：使用cssModules的方式编译，类似指定loader，需要用$style对象获取
   .main-header
     text-align center
 
