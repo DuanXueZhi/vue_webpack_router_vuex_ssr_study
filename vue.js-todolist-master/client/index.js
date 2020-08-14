1a17635e176856c75d2be7cbf95d71c49f81734a
DuanXueZhi <38554820+DuanXueZhi@users.noreply.github.com>
2020-08-14 15:40:37 +0800

    vue：vue-router全局路由守卫

--- vue.js-todolist-master/client/index.js
+++ vue.js-todolist-master/client/index.js
@@ -15,6 +15,28 @@ Vue.use(VueRouter)
 
 const router = createRouter()
 
+/**
+ * 全局路由守卫
+ */
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach invoked')
+  // if (to.fullPath === '/app') {
+  //   next({ name: '/login', replace: true }) // replace是否推送到路由栈中储存
+  // } else {
+  next()
+  // }
+})
+
+// 全局 beforeEach -> 路由 beforeEnter -> 组件 beforeEnter -> beforeResolve
+router.beforeResolve((to, from, next) => {
+  console.log('beforeResolve invoked')
+  next()
+})
+
+router.afterEach((to, from) => {
+  console.log('afterEach invoked')
+})
+
 new Vue({
   router, // 注入
   render: (h) => h(App)
